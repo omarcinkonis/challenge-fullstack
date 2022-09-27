107a38a574a6a5d181abf77148b8a5dff78f7084
Osmundas <osmundas.m@gmail.com>
2022-09-27 14:42:38 +0300

    Auth

---
+++ resources/views/components/content.blade.php
@@ -0,0 +1,9 @@
+<div id="navigation">
+    <ul>
+        <li><a href="index.php">Login</a></li>
+        <li><a href="about.php">Register</a></li>
+    </ul>
+</div>
+<div id="content">
+    {{ $slot }}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('users', UserController::class);
+
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
